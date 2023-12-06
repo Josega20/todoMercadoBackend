d5781cf5e08d3282e458933e5a6f7c68a7f16a76
Josega20 <122412778+Josega20@users.noreply.github.com>
2023-12-06 00:01:56 -0300

    mejora 1 de estructrura

--- consultas.js
+++ consultas.js
@@ -9,37 +9,5 @@ const pool = new Pool({
   allowExitOnIdle: true,
 });
 
-const verificarUsuario = async (email, password) => {
-  const consulta = "SELECT * FROM usuarios where email = $1";
-  const {
-    rows: [usuario],
-    rowCount,
-  } = await pool.query(consulta, [email]);
-  const { password: passwordEncriptada } = usuario;
-  const passwordCorrecta = bcrypt.compareSync(password, passwordEncriptada);
-  if (!passwordCorrecta || !rowCount){
-    throw {code: 401, message: "Usuario o contraseña incorrecto"}
-  }
-};
-const obtenerDatosUsuario = async (email) =>{
-const consulta = "SELECT * FROM usuarios where email = $1";
-  const {
-    rows: [usuario],
-    rowCount,
-  } = await pool.query(consulta, [email]);
-  const { password: passwordEncriptada } = usuario;
-   if (!rowCount){
-    throw {code: 404, message: "usuario no encontrado"}
-  }
-  delete usuario.password;
-  return usuario;
-};
-
-const agregarUsuario = async ( nombre,email, password,telefono) =>{
-    const passwordEncriptada = bcrypt.hashSync(password)
-    const values = [nombre,email, passwordEncriptada, telefono]
-    const consulta= "INSERT INTO usuarios values (DEFAULT, $1, $2, $3, $4)";
-    await pool.query(consulta, values);
-}
 
 module.exports = { verificarUsuario, obtenerDatosUsuario, agregarUsuario};
