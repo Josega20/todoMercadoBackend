3e9a3289920d0d67833c621b0048cb4cf59ffb87
Josega20 <122412778+Josega20@users.noreply.github.com>
2023-12-08 22:44:31 -0300

    post usuarios

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,7 +1,5 @@
-//consultas a la base datos para el usuario
-
 const bcrypt = require("bcryptjs");
-const { pool } = require("../db");
+const { pool } = require("../db")
 
 //verifica el usuario y lo valida 
 const verificarUsuario = async (email, password) => {
@@ -13,11 +11,9 @@ const verificarUsuario = async (email, password) => {
   const { password: passwordEncriptada } = usuario;
   const passwordCorrecta = bcrypt.compareSync(password, passwordEncriptada);
   if (!passwordCorrecta || !rowCount) {
-    throw { code: 401, message: "Usuario o contraseña incorrecto" }
+    throw { code: 401, message: "Usuario o contraseña incorrecto" };
   }
 };
-
-//obetiene los datos del usuario
 const obtenerDatosUsuario = async (email) => {
   const consulta = "SELECT * FROM usuarios where email = $1";
   const {
@@ -26,38 +22,26 @@ const obtenerDatosUsuario = async (email) => {
   } = await pool.query(consulta, [email]);
   const { password: passwordEncriptada } = usuario;
   if (!rowCount) {
-    throw { code: 404, message: "usuario no encontrado" }
+    throw { code: 404, message: "usuario no encontrado" };
   }
   delete usuario.password;
   return usuario;
 };
 
-//se agrega un nuevo usuario
 const agregarUsuario = async (nombre, email, password, telefono) => {
-  // Verificar si el correo ya existe
-  const verificarCorreo = "SELECT * FROM usuarios WHERE email = $1";
-  const { rowCount: existeCorreo } = await pool.query(verificarCorreo, [email]);
-
-  if (existeCorreo > 0) { //si existe un correo es mayor a 0 lanza el error
-    // El correo ya está registrado
-    throw { code: 409, message: "El correo ya está registrado" };//creo que este es el codigo
-  }
-
-  const passwordEncriptada = bcrypt.hashSync(password)
-  const values = [nombre, email, passwordEncriptada, telefono]
+  const passwordEncriptada = bcrypt.hashSync(password);
+  console.log(passwordEncriptada);
+  const values = [nombre, email, passwordEncriptada, telefono];
   const consulta = "INSERT INTO usuarios values (DEFAULT, $1, $2, $3, $4)";
-  await pool.query(consulta, values);
+  try {
+    await pool.query(consulta, values);
+} catch (error) {
+    console.error("Error during user insertion:", error);
+    throw error;
 }
 
-//funcion para eliminar un usuario
-const eliminarUsuario = async (email) => {
-  const eliminarUsuarioQuery = "DELETE FROM usuarios WHERE email = $1";
-  await pool.query(eliminarUsuarioQuery, [email]);
+ // console.log(consulta);
+  //await pool.query(consulta, values);
 };
 
-module.exports = { 
-verificarUsuario, 
-obtenerDatosUsuario, 
-agregarUsuario, 
-eliminarUsuario, 
-};
+module.exports = { verificarUsuario, obtenerDatosUsuario, agregarUsuario };
--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const rutas = require("./rutas.js")
 const cors = require("cors");
+//const{chequearCredenciales, verificarToken,reportarSolicitudes, reportarSolicitudesRegistro} =require ("./middlewares")
 const app = express();
 const PORT = 3000;
 app.listen(PORT, console.log(`¡Servidor ${PORT} encendido!`));
 app.use(express.json());
 app.use(cors());
-app,use(rutas)
+const {
+  verificarUsuario,
+  obtenerDatosUsuario,
+  agregarUsuario,
+} = require("./controllers/usuarios.js");
+
+app.post("/registro",/*reportarSolicitudesRegistro,*/ cors(), async (req, res) => {
+  try {
+    const {nombre,email, password, telefono} = req.body;
+    console.log(req.body);
+    await agregarUsuario(nombre,email, password, telefono);
+    res.send("Usuario creado con exito");
+  } catch (error) {
+    res.status(500).send(error);
+    console.log("no es posible ejecutar el requerimiento");
+  }
+});
