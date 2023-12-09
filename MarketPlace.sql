de83f9ef91ef595c2d774cf410bb6d3af77a6952
Ariel Stancovich <arielstancovichsuarez@gmail.com>
2023-12-08 23:28:09 -0300

    update
    se arregla tabla?

--- MarketPlace.sql
+++ MarketPlace.sql
@@ -7,16 +7,16 @@ CREATE DATABASE market_place;
 -- Crear la tabla "usuarios"
 CREATE TABLE usuarios (
     id_usuario SERIAL PRIMARY KEY,
-    nombre VARCHAR(50),
-    email VARCHAR(100),
-    password VARCHAR(16),
+    nombre VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255),
     telefono INTEGER
 );
 
 -- Crear la tabla "productos"
 CREATE TABLE productos (
     id_producto SERIAL PRIMARY KEY,
-    nombre_producto VARCHAR(50),
+    nombre_producto VARCHAR(255),
     descripcion VARCHAR(255),
     precio INTEGER,
     id_usuario INTEGER REFERENCES usuarios(id_usuario),
