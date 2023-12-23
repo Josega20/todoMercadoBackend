619e30b3d3737ee60968d2e54aa7171abb6ce492
Ariel Stancovich <arielstancovichsuarez@gmail.com>
2023-12-22 23:35:44 -0300

    update db

--- db.js
+++ db.js
@@ -2,10 +2,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  password: "16894832_Jg", //"Knight123"
-  database: "market_place",
+  host: "dpg-cm34cjocmk4c73c9qmfg-a",
+  port: 5432,
+  user: "admin",
+  password: "EK6pzrqAqthT1tYE5gPMQ5KRLLR88Kxr", //"Knight123"
+  database: "market_place_isof",
   allowExitOnIdle: true,
 });
 
